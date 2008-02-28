607dfb2cc485a70d960202eeabe1e42487322f2e
marnen <marnen@a52df236-a610-0410-a8a4-ba015fd774c5>
2008-02-28 21:08:08 +0000

    Add validations to states and countries (Mingle #14).
    git-svn-id: http://svn.ebon-askavi.homedns.org:8080/marnen/quorum2/trunk@1340 a52df236-a610-0410-a8a4-ba015fd774c5

--- app/models/state.rb
+++ app/models/state.rb
@@ -1,2 +1,6 @@
 class State < ActiveRecord::Base
+  belongs_to :country
+  validates_presence_of :country_id
+  validates_presence_of :code
+  validates_presence_of :name
 end
--- spec/models/state_spec.rb
+++ spec/models/state_spec.rb
@@ -1,11 +1,34 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
-describe State do
+describe State, "(general properties)" do
+  it "should belong to a Country" do
+    State.reflect_on_association(:country).macro.should == :belongs_to
+  end
+end
+
+describe State, "(validations)" do
   before(:each) do
     @state = State.new
+    @state.name = "xyz" # arbitrary value
+    @state.country_id = 23 # arbitrary value
+    @state.code = "AA" # arbitrary value
   end
-
-  it "should be valid" do
+  
+  it "should not be valid without a state code" do
     @state.should be_valid
+    @state.code = nil
+    @state.should_not be_valid
+  end
+  
+  it "should not be valid without a name" do
+    @state.should be_valid
+    @state.name = nil
+    @state.should_not be_valid
+  end
+  
+  it "should not be valid without a country" do
+    @state.should be_valid
+    @state.country = nil
+    @state.should_not be_valid
   end
 end
