dccb7b065022a7cf9d9872a5c888f21ad00cda58
marnen <marnen@a52df236-a610-0410-a8a4-ba015fd774c5>
2008-02-28 18:40:28 +0000

    Start adding validations to Event (Mingle #14).
    git-svn-id: http://svn.ebon-askavi.homedns.org:8080/marnen/quorum2/trunk@1336 a52df236-a610-0410-a8a4-ba015fd774c5

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,25 @@
 class Event < ActiveRecord::Base
-  belongs_to :state
+  belongs_to :state, :include => :country
+  has_and_belongs_to_many :users
+  validates_presence_of :state_id
+=begin
+  def coords
+    c = self[:coords]
+    if (c.nil?)
+      address_to_code = "#{street}, #{city}, #{state.code}, #{zip}, #{country.code}"
+      begin
+        geo = Geocoding::get(address_to_code)
+        if geo.status == Geocoding::GEO_SUCCESS
+          c = write_attribute(:coords, Point.from_coordinates(geo[0].lonlat))
+        else
+          raise "Geocoding failed with code #{geo.status} for #{address_to_code}"
+        end
+      rescue
+        write_attribute(:coords, nil)
+        c = Point.from_x_y(0, 0)
+      end
+    end
+   c
+  end
+=end
 end
--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,15 +1,51 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
-describe Event do
+describe Event, "(general properties)" do
   before(:each) do
-    @event = Event.new
-  end
-
-  it "should be valid" do
-    @event.should be_valid
   end
   
   it "should belong to a State" do
     Event.reflect_on_association(:state).macro.should == :belongs_to
   end
+  
+  it "should belong to a Country" do
+    pending("should test for :include :country on :state, but I'm not sure how to write that.")
+  end
+  
+  it "should have and belong to many Users" do
+    Event.reflect_on_association(:users).macro.should == :has_and_belongs_to_many
+  end
 end
+
+describe Event, "(validations)" do
+  before(:each) do
+    @event = Event.new
+  end
+  
+  it "should be invalid if state is nil" do
+    @event.should_not be_valid
+  end
+  
+  it "should be valid if state is not nil" do
+    @event.state_id = 23 # arbitrary; should be able to use any value
+    @event.should be_valid
+  end
+end
+=begin
+describe Event, "(geographical features)" do
+  before(:each) do
+    Geocoding::Placemarks.any_instance.expects(:[]).returns(Geocoding::Placemark.new)
+    Geocoding::Placemark.any_instance.stubs(:latlon).returns([1.0, 2.0])
+    Geocoding.expects(:get).returns(Geocoding::Placemarks.new('Test Placemarks', Geocoding::GEO_SUCCESS))
+    Point::any_instance.stubs(:from_coordinates).returns(true)
+
+    @event = Event.new
+  end
+
+  it "should have coords (Point)" do
+    @event.should respond_to(:coords)
+    @event.coords.should_not be_nil
+    @event.coords.should be_a_kind_of(Point)
+  end
+end
+=end
