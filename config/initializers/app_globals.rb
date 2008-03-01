ba3df1b70920a196eeb8312c6d43557743de6448
marnen <marnen@a52df236-a610-0410-a8a4-ba015fd774c5>
2008-03-01 05:19:12 +0000

    Add domain name and iCal date format (Mingle #14).
    git-svn-id: http://svn.ebon-askavi.homedns.org:8080/marnen/quorum2/trunk@1357 a52df236-a610-0410-a8a4-ba015fd774c5

--- config/initializers/app_globals.rb
+++ config/initializers/app_globals.rb
@@ -1,3 +1,9 @@
 # Application globals go here.
 
 SITE_TITLE = "Quorum" # Name of site as it appears in <title> element
+
+DOMAIN = "quorum2.ebon-askavi.homedns.org" # Domain on which the site is hosted
+
+module ActiveSupport::CoreExtensions::Date::Conversions
+  DATE_FORMATS[:ical] = "%Y%m%d" # yyyymmdd, for iCal conversion
+end
