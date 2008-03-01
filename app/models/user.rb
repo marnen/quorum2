2c9f204e9dbe5e3367dc2856c15b24d404f5dd82
marnen <marnen@a52df236-a610-0410-a8a4-ba015fd774c5>
2008-03-01 18:32:09 +0000

    Flesh out User model (Mingle #15).
    git-svn-id: http://svn.ebon-askavi.homedns.org:8080/marnen/quorum2/trunk@1369 a52df236-a610-0410-a8a4-ba015fd774c5

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ActiveRecord::Base
+  belongs_to :state
+  has_many :commitments
+  has_many :events, :through => :commitments
+  validates_presence_of :email
+  validates_length_of :md5_password, :is => 32
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,11 +1,44 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
-describe User do
+describe User, "(general properties)" do
+  before(:each) do
+  end
+  
+  it "should belong to a State" do
+    User.reflect_on_association(:state).macro.should == :belongs_to
+  end
+  
+  it "should have many Commitments" do
+    User.reflect_on_association(:commitments).macro.should == :has_many
+  end
+  
+  it "should have many Events through Commitments" do
+    reflection = User.reflect_on_association(:events)
+    reflection.macro.should == :has_many
+    reflection.options.should have_key :through
+    reflection.options[:through].should == :commitments
+  end
+end
+  
+describe User, "(validations)" do
   before(:each) do
     @user = User.new
+    @user.email = "foo@bar.com" # arbitrary value
+    @user.md5_password = "0123456789abcdef0123456789abcdef" # arbitrary 32-byte string
+    @user
   end
 
-  it "should be valid" do
+  it "should not be valid without an e-mail address" do
     @user.should be_valid
+    @user.email = nil
+    @user.should_not be_valid
+  end
+  
+  it "should not be valid without a password hash of length exactly 32" do
+    @user.should be_valid
+    @user.md5_password <<= "0" # length 33
+    @user.should_not be_valid
+    @user.md5_password = nil
+    @user.should_not be_valid
   end
 end
