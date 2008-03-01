07c0253f62888ec1576ad90be61d0e66d4968f76
marnen <marnen@a52df236-a610-0410-a8a4-ba015fd774c5>
2008-03-01 05:18:08 +0000

    Add method to create iCal UID (Mingle #14).
    git-svn-id: http://svn.ebon-askavi.homedns.org:8080/marnen/quorum2/trunk@1354 a52df236-a610-0410-a8a4-ba015fd774c5

--- app/helpers/event_helper.rb
+++ app/helpers/event_helper.rb
@@ -1,2 +1,7 @@
 module EventHelper
+
+  def ical_uid(event)
+    "event-" << event.id.to_s << "@" << DOMAIN
+  end
+
 end
--- spec/helpers/event_helper_spec.rb
+++ spec/helpers/event_helper_spec.rb
@@ -1,11 +1,17 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
 describe EventHelper do
-  
+  it "should generate an iCal unique id as a String" do
+    event = Event.new
+    ical_uid(event).should_not be_nil
+    ical_uid(event).should be_a_kind_of(String)
+  end
+=begin
   #Delete this example and add some real ones or delete this file
   it "should include the EventHelper" do
     included_modules = self.metaclass.send :included_modules
     included_modules.should include(EventHelper)
   end
+=end
   
 end
