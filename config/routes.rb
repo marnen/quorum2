d06c7df54831d1fb17527180cbc165563df9ebaf
marnen <marnen@a52df236-a610-0410-a8a4-ba015fd774c5>
2008-03-10 20:39:36 +0000

    Remove Rails splash page.
    git-svn-id: http://svn.ebon-askavi.homedns.org:8080/marnen/quorum2/trunk@1420 a52df236-a610-0410-a8a4-ba015fd774c5

--- config/routes.rb
+++ config/routes.rb
@@ -35,6 +35,7 @@ ActionController::Routing::Routes.draw do |map|
 
   # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
   # map.root :controller => "welcome"
+  map.root :controller => 'event', :action => 'list'
 
   # See how all your routes lay out with "rake routes"
 
