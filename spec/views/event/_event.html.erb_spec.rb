3a68b1cf8d93ceb5a05517e9f7da8ad20491deb6
marnen <marnen@a52df236-a610-0410-a8a4-ba015fd774c5>
2008-04-01 21:11:15 +0000

    Fix specs for refactored partial (Mingle #33).
    git-svn-id: http://svn.ebon-askavi.homedns.org:8080/marnen/quorum2/trunk@1469 a52df236-a610-0410-a8a4-ba015fd774c5

--- spec/views/event/_event.html.erb_spec.rb
+++ spec/views/event/_event.html.erb_spec.rb
@@ -9,119 +9,95 @@ describe 'event/_event' do
 
   before(:all) do
     @event_orig = Event.find(:first).clone
-    user = User.find(:first)
+    @user = User.find(:first)
   end
   
   before(:each) do
     @event = @event_orig.clone
-    User.stub!(:current_user).and_return(user)
+    User.stub!(:current_user).and_return(@user)
   end
 
-  it "should contain an edit link for all events, if the current user is an admin" do
+  it "should contain an edit link for the event, if the current user is an admin" do
     role = mock_model(Role, :name => 'admin')
     admin = mock_model(User, :role => role)
     User.stub!(:current_user).and_return(admin)
-    render 'event/list'
-    for event in @events do
-      url = url_for(:controller => 'event', :action => 'edit', :id => event.id, :escape => false)
-      response.should have_tag("#event_#{event.id} a[href=" << url << "]")
-    end
+    render_view
+    url = url_for(:controller => 'event', :action => 'edit', :id => @event.id, :escape => false)
+    response.should have_tag("#event_#{@event.id} a[href=" << url << "]")
   end
 
-  it "should contain a delete link for each event, if the current user has an admin account" do
+  it "should contain a delete link for the event, if the current user has an admin account" do
     role = mock_model(Role, :name => 'admin')
     admin = mock_model(User, :role => role)
     User.stub!(:current_user).and_return(admin)
-    render 'event/list'
-    for event in @events do
-      url = url_for(:controller => 'event', :action => 'delete', :id => event.id, :escape => false)
-      response.should have_tag("#event_#{event.id} a[href=" << url << "]")
-    end
+    render_view
+    url = url_for(:controller => 'event', :action => 'delete', :id => @event.id, :escape => false)
+    response.should have_tag("#event_#{@event.id} a[href=" << url << "]")
   end
   
-  it "should have a control to set the current user's attendance for each event" do
+  it "should have a control to set the current user's attendance for the event" do
     template.expect_render(:partial => 'attendance', :locals => {:event => @event})
     render_view
   end
   
-  it "should contain a distance in miles or km for each event with good coords" do
+  it "should contain a distance in miles or km for the event if it has good coords" do
     render_view
     if !@event.coords.nil? then
       response.should have_tag("#event_#{@event.id} .distance", /\d (miles|km)/)
     end
   end
   
-  it "should show a name for each Event in a tag of class 'summary'" do
-    render 'event/list'
-    for event in @events do
-      response.should have_tag("#event_#{event.id} .summary", h(event.name))
-    end
+  it "should show the name of the event in a tag of class 'summary'" do
+    render_view
+    response.should have_tag("#event_#{@event.id} .summary", h(@event.name))
   end 
 
-  it "should show a site for each Event" do
-   render 'event/list'
-   for event in @events do
-      response.should have_tag("#event_#{event.id}", /#{h(event.site)}/)
-    end
+  it "should show the site for the event" do
+    render_view
+    response.should have_tag("#event_#{@event.id}", /#{h(@event.site)}/)
   end 
 
-  it "should show a street address for each Event in a tag of class 'street-address" do
-    render 'event/list'
-    for event in @events do
-      response.should have_tag("#event_#{event.id} .street-address", /#{[h(event.street), h(event.street2)].join('.*')}/m)
-    end
+  it "should show the street address of the event in a tag of class 'street-address" do
+    render_view
+    response.should have_tag("#event_#{@event.id} .street-address", /#{[h(@event.street), h(@event.street2)].join('.*')}/m)
   end
   
-  it "should show a city for each event in a tag of class 'locality'" do
-    render 'event/list'
-    for event in @events do
-      response.should have_tag("#event_#{event.id} .locality", h(event.city))
-    end
+  it "should show the city of the event in a tag of class 'locality'" do
+    render_view
+    response.should have_tag("#event_#{@event.id} .locality", h(@event.city))
   end
   
-  it "should show a state code for each event in a tag of class 'region'" do
-    render 'event/list'
-    for event in @events do
-      response.should have_tag("#event_#{event.id} .region", h(event.state.code))
-    end
+  it "should show the state code of the event in a tag of class 'region'" do
+    render_view
+    response.should have_tag("#event_#{@event.id} .region", h(@event.state.code))
   end
 
-  it "should show a country code for each event in a tag of class 'country-name'" do
-    render 'event/list'
-    for event in @events do
-      response.should have_tag("#event_#{event.id} .country-name", h(event.state.country.code))
-    end
+  it "should show the country code of each event in a tag of class 'country-name'" do
+    render_view
+    response.should have_tag("#event_#{@event.id} .country-name", h(@event.country.code))
   end
   
-  it "should show a map link for each event" do
-    render 'event/list'
-    for event in @events do
-      url = url_for(:controller => 'event', :action => 'map', :id => event.id, :escape => false)
-      response.should have_tag("#event_#{event.id} a[href=" << url << "]")
-    end
+  it "should show a map link for the event" do
+    render_view
+    url = url_for(:controller => 'event', :action => 'map', :id => @event.id, :escape => false)
+    response.should have_tag("#event_#{@event.id} a[href=" << url << "]")
   end
   
-  it "should show an iCal export link for each event, of class 'ical'" do
-    render 'event/list'
-    for event in @events do
-      url = url_for(:controller => 'event', :action => 'export', :id => event.id, :escape => false)
-      response.should have_tag("#event_#{event.id} a.ical[href=" << url << "]")
-    end
+  it "should show an iCal export link for the event, of class 'ical'" do
+    render_view
+    url = url_for(:controller => 'event', :action => 'export', :id => @event.id, :escape => false)
+    response.should have_tag("#event_#{@event.id} a.ical[href=" << url << "]")
   end
   
-  it "should show a date for each event in RFC 822 format, wrapped in an <abbr> of class 'dtstart' with ical-style date as the title" do
-    render 'event/list'
-    for event in @events do
-      date = event.date
-      response.should have_tag("#event_#{event.id} abbr.dtstart[title=" << date.to_formatted_s(:ical) << "]", date.to_formatted_s(:rfc822))
-    end
+  it "should show a date for the event in RFC 822 format, wrapped in an <abbr> of class 'dtstart' with ical-style date as the title" do
+    render_view
+    date = @event.date
+    response.should have_tag("#event_#{@event.id} abbr.dtstart[title=" << date.to_formatted_s(:ical) << "]", date.to_formatted_s(:rfc822))
   end
   
   it "should have an element of class 'uid' for each event, containing an iCal unique ID for the event" do
-    render 'event/list'
-    for event in @events do
-      response.should have_tag("#event_#{event.id} .uid", ical_uid(event))
-    end
+    render_view
+    response.should have_tag("#event_#{@event.id} .uid", ical_uid(@event))
   end
   
   it "should contain an edit link for each event that the current user created" do
@@ -131,7 +107,7 @@ describe 'event/_event' do
     nonadmin = mock_model(User, :role => user)
     User.stub!(:current_user).and_return(nonadmin) # non-admin
     Event.stub!(:created_by).and_return(nonadmin)
-    render 'event/list'
+    render_view
     for event in @events do
       User.current_user.should == nonadmin
       event.created_by.should == nonadmin
