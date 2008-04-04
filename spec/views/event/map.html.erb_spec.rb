2febb984a95c5c4c609569bf6a47593204939b66
marnen <marnen@a52df236-a610-0410-a8a4-ba015fd774c5>
2008-04-04 21:52:37 +0000

    Refactor spec code, since #36 broke some of it.
    git-svn-id: http://svn.ebon-askavi.homedns.org:8080/marnen/quorum2/trunk@1476 a52df236-a610-0410-a8a4-ba015fd774c5

--- spec/views/event/map.html.erb_spec.rb
+++ spec/views/event/map.html.erb_spec.rb
@@ -8,6 +8,7 @@ describe "/event/map" do
   end
   
   it "should render the map in @map" do
+    User.stub!(:current_user).and_return(mock_model(User, :address_for_geocoding => 'Arbitrary Address, Somewhere, NY, US'))
     render 'event/map'
     response.should have_tag("#map")
   end
