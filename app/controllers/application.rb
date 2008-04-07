318fa9fb115b4367adbdcc36773f6124de1dfac9
marnen <marnen@a52df236-a610-0410-a8a4-ba015fd774c5>
2008-04-07 18:59:04 +0000

    Comment out secret so that #26 and #18 work properly.
    git-svn-id: http://svn.ebon-askavi.homedns.org:8080/marnen/quorum2/trunk@1479 a52df236-a610-0410-a8a4-ba015fd774c5

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -13,7 +13,7 @@ class ApplicationController < ActionController::Base
 
   # See ActionController::RequestForgeryProtection for details
   # Uncomment the :secret if you're not using the cookie session store
-  protect_from_forgery :secret => 'efb0994fc16e17d478432d89deb46862'
+  protect_from_forgery # :secret => 'efb0994fc16e17d478432d89deb46862'
   
   # Add gettext code for i18n, from http://manuals.rubyonrails.com/read/chapter/105
   init_gettext "quorum", "UTF-8", "text/html"
