e7c54519b690bede202fa6334941d7091967efbc
marnen <marnen@a52df236-a610-0410-a8a4-ba015fd774c5>
2008-10-15 20:49:48 +0000

    Make user_id into an integer. Why was it ever anything else?
    git-svn-id: http://svn.ebon-askavi.homedns.org:8080/marnen/quorum2/trunk@1604 a52df236-a610-0410-a8a4-ba015fd774c5

--- db/migrate/003_create_attendance_schema.rb
+++ db/migrate/003_create_attendance_schema.rb
@@ -2,7 +2,7 @@ class CreateAttendanceSchema < ActiveRecord::Migration
   def self.up
     create_table :events_users do |t|
       t.integer :event_id, :null => false
-      t.string :user_id, :null => false
+      t.integer :user_id, :null => false
       t.boolean :status
     end
   end
