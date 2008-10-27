2b21257ebc4c5348d282f7434048710dcaab422d
marnen <marnen@a52df236-a610-0410-a8a4-ba015fd774c5>
2008-10-27 21:07:57 +0000

    Fix UserMailer to use reasonable values.
    git-svn-id: http://svn.ebon-askavi.homedns.org:8080/marnen/quorum2/trunk@1656 a52df236-a610-0410-a8a4-ba015fd774c5

--- app/models/user_mailer.rb
+++ app/models/user_mailer.rb
@@ -3,21 +3,21 @@ class UserMailer < ActionMailer::Base
     setup_email(user)
     @subject    += 'Please activate your new account'
   
-    @body[:url]  = "http://YOURSITE/activate/#{user.activation_code}"
+    @body[:url]  = "http://#{DOMAIN}/activate/#{user.activation_code}"
   
   end
   
   def activation(user)
     setup_email(user)
     @subject    += 'Your account has been activated!'
-    @body[:url]  = "http://YOURSITE/"
+    @body[:url]  = "http://#{DOMAIN}/"
   end
   
   protected
     def setup_email(user)
       @recipients  = "#{user.email}"
-      @from        = "ADMINEMAIL"
-      @subject     = "[YOURSITE] "
+      @from        = EMAIL
+      @subject     = "[#{SITE_TITLE}] "
       @sent_on     = Time.now
       @body[:user] = user
     end
