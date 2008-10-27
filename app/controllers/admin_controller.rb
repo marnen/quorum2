680dbaeba6589d5639e09fdc6cfd0f2628821bdc
marnen <marnen@a52df236-a610-0410-a8a4-ba015fd774c5>
2008-10-27 19:45:26 +0000

    Display multiple calendars correctly in admin interface.
    git-svn-id: http://svn.ebon-askavi.homedns.org:8080/marnen/quorum2/trunk@1647 a52df236-a610-0410-a8a4-ba015fd774c5

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -4,7 +4,7 @@ class AdminController < ApplicationController
   
   def index
     @page_title = _("Administrative tools")
-    @calendars = User.current_user.permissions.find_by_role_id(admin, :include => :calendar).to_a.collect{|p| p.calendar}
+    @calendars = User.current_user.permissions.find_all_by_role_id(admin, :include => :calendar).to_a.collect{|p| p.calendar}.sort{|x, y| x.name <=> y.name}
   end
   
 end
