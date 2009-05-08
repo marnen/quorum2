09109189decf15facc9cf3acd2c4d3e531d8255b
Marnen Laibow-Koser <marnen@marnen.org>
2009-05-08 16:53:23 -0400

    Rewrite test helper to use literal user objects, not fixture keys. [#22]

--- lib/authenticated_test_helper.rb
+++ lib/authenticated_test_helper.rb
@@ -1,10 +1,10 @@
 module AuthenticatedTestHelper
-  # Sets the current user in the session from the user fixtures.
+  # Sets the current user in the session.
   def login_as(user)
-    @request.session[:user_id] = user ? users(user).id : nil
+    @request.session[:user_id] = user ? user.id : nil
   end
 
   def authorize_as(user)
-    @request.env["HTTP_AUTHORIZATION"] = user ? ActionController::HttpAuthentication::Basic.encode_credentials(users(user).login, 'test') : nil
+    @request.env["HTTP_AUTHORIZATION"] = user ? ActionController::HttpAuthentication::Basic.encode_credentials(user.login, 'test') : nil
   end
 end
