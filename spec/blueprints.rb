09daef0ae3dd3e7a8969083a5140c4b083c86bde
Marnen Laibow-Koser <marnen@marnen.org>
2009-05-08 15:35:41 -0400

    Set up factories and shams for Event and linked classes. [#22]

--- spec/blueprints.rb
+++ spec/blueprints.rb
@@ -1,2 +1,31 @@
 # Object blueprints for Machinist.
 require 'faker'
+LETTERS = ('A'..'Z').to_a
+
+Event.blueprint do
+  name {Sham.event_name}
+  state {State.make}
+  calendar {Calendar.make}
+end
+
+State.blueprint do
+  country {Country.make}
+  name {Sham.generic_name}
+  code {Sham.code}
+end
+
+Country.blueprint do
+  name {Sham.generic_name}
+  code {Sham.code}
+end
+
+Calendar.blueprint do
+  name {Sham.calendar_name}
+end
+
+Sham.define do
+  calendar_name {Faker::Name.name + "'s calendar"}
+  generic_name {Faker::Name.last_name}
+  code {LETTERS.rand + LETTERS.rand} # 2 random letters
+  event_name {Faker::Lorem.words(3).join(' ')}
+end
