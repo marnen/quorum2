72ff9d06f05ac28424dfde052a5fdc672c8913a5
Marnen Laibow-Koser <marnen@marnen.org>
2009-05-08 15:04:29 -0400

    Add Faker dependency. [#22]

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -26,3 +26,6 @@ config.gem "rspec", :lib => false, :version => ">=1.2.2"
 config.gem "rspec-rails", :lib => false, :version => ">=1.2.2"  
 config.gem "webrat", :lib => false, :version => ">=0.4.3"  
 config.gem "cucumber", :lib => false, :version => ">=0.2.2"
+
+# For Machinist...
+config.gem 'faker'
