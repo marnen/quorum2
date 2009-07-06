2df33fd344dd9d08787035e3619eca02995132e8
Marnen Laibow-Koser <marnen@marnen.org>
2009-07-06 18:50:29 -0400

    Refactor the Address constructor somewhat.

--- app/models/address.rb
+++ app/models/address.rb
@@ -9,27 +9,19 @@ class Address
   
   # Initializes a new #Address object from a #Hash of fields or separate arguments in the order given in FIELDS.
   def initialize(*args)
-    opts = {}
     case args.length
       when 0
         # do nothing
       when 1
         if args[0].kind_of?(Hash)
-          opts = args[0]
+          set_from_hash args[0]
         else
           raise ArgumentError, "expected Hash, got #{args[0].class.name}"
         end
       when FIELDS.length
-        FIELDS.each_index do |i|
-          opts[FIELDS[i]] = args[i]
-        end
+        set_from_array args
       else
         raise ArgumentError, "expected 0, 1, or #{FIELDS.length} arguments, got #{args.length}"
-    end
-    FIELDS.each do |f|
-      if opts.has_key?(f)
-        eval "@#{f.to_s} = opts[:#{f.to_s}]"
-      end
     end
     if !@state.blank? and !@state.kind_of?(State)
       @state = State.find(@state)
@@ -68,5 +60,27 @@ class Address
     else
       return false
     end
+  end
+  
+  protected
+  
+  # TODO: set_from_* methods should perhaps become public.
+  
+  # Sets instance variables from the supplied #Hash.
+  def set_from_hash(hash)
+    FIELDS.each do |f|
+      if hash.has_key?(f)
+        eval "@#{f.to_s} = hash[:#{f.to_s}]"
+      end
+    end
+  end
+  
+  # Sets instance variables from the supplied #Array, in the same order as #FIELDS.
+  def set_from_array(array)
+    hash = {}
+    FIELDS.each_index do |i|
+      hash[FIELDS[i]] = array[i]
+    end
+    set_from_hash hash
   end
 end
