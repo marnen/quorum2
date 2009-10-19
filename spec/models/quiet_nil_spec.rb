dcf5d67a5b69e33648edebb7655486732f39f9d1
Marnen Laibow-Koser <marnen@marnen.org>
2009-10-19 18:51:06 -0400

    Introduce QuietNil class for when we really don't care about chained methods. [#41]

---
+++ spec/models/quiet_nil_spec.rb
@@ -0,0 +1,29 @@
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+
+describe QuietNil do
+  it "should be a valid class" do
+    QuietNil.class.should == Class
+  end
+  
+  it "should respond as nil to predefined methods" do
+    qn = QuietNil.instance
+    qn.should be_nil
+    pending "can't figure out how to convert to boolean" do
+      (!!qn).should be_false
+    end
+    qn.to_s.should == nil.to_s
+    qn.to_a.should == nil.to_a
+  end
+  
+  it "should not whine when it receives methods that aren't defined on nil" do
+    qn = QuietNil.instance
+    p = lambda {qn.foobarbaz(1, 2, 3)}
+    p.should_not raise_error
+    
+    # We're overriding equality, so we can't do
+    # (p.call.equal? qn).should be_true
+    # Instead:
+    p.call.should be_nil
+    lambda {p.call.quux(:quuuux)}.should_not raise_error
+  end
+end
