b324bea04434d42327532adad05995aefa7008b3
Marnen Laibow-Koser <marnen@marnen.org>
2009-10-22 00:38:07 -0400

    Start putting code from AddressUtilities into acts_as_addressed. [#44]

--- vendor/plugins/acts_as_addressed/lib/acts_as_addressed.rb
+++ vendor/plugins/acts_as_addressed/lib/acts_as_addressed.rb
@@ -6,6 +6,10 @@ module Acts #:nodoc:
     end
     
     module ClassMethods
+      # Includes the acts_as_addressed structure in the model it's called on.
+      def acts_as_addressed
+        composed_of :address, :mapping => %w(street street2 city state_id zip coords).collect{|x| [x, x.gsub(/_id$/, '')]}
+      end
     end
   end
 end
--- vendor/plugins/acts_as_addressed/spec/acts_as_addressed_spec.rb
+++ vendor/plugins/acts_as_addressed/spec/acts_as_addressed_spec.rb
@@ -11,7 +11,20 @@ end
 describe Acts::Addressed::ClassMethods do
   describe "acts_as_addressed" do
     it "should be a valid method" do
+      Acts::Addressed::ClassMethods.instance_method(:acts_as_addressed).should_not be_nil
+    end
+    
+    describe "effects on model" do
+      before :each do
+        @base = Class.new
+        @base.send :include, Acts::Addressed
+        @class = Class.new @base
+      end
       
+      it "should create an Address aggregation" do
+        @class.should_receive(:composed_of).with(:address, :mapping => %w(street street2 city state_id zip coords).collect{|x| [x, x.gsub(/_id$/, '')]})
+        @class.acts_as_addressed
+      end
     end
   end
 end
