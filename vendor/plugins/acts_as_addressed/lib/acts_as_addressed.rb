ddfcf813cf0775b1c6e18063e5cf11fb967a2c6d
Marnen Laibow-Koser <marnen@marnen.org>
2009-10-22 01:07:43 -0400

    Bring over #country from AddressUtilities and create instance method infrastructure. [#44]

--- vendor/plugins/acts_as_addressed/lib/acts_as_addressed.rb
+++ vendor/plugins/acts_as_addressed/lib/acts_as_addressed.rb
@@ -9,6 +9,14 @@ module Acts #:nodoc:
       # Includes the acts_as_addressed structure in the model it's called on.
       def acts_as_addressed
         composed_of :address, :mapping => %w(street street2 city state_id zip coords).collect{|x| [x, x.gsub(/_id$/, '')]}
+        include InstanceMethods
+      end
+    end
+    
+    module InstanceMethods
+      # Nil-safe country accessor.
+      def country
+        address.country
       end
     end
   end
--- vendor/plugins/acts_as_addressed/spec/acts_as_addressed_spec.rb
+++ vendor/plugins/acts_as_addressed/spec/acts_as_addressed_spec.rb
@@ -19,12 +19,43 @@ describe Acts::Addressed::ClassMethods do
         @base = Class.new
         @base.send :include, Acts::Addressed
         @class = Class.new @base
+        @class.stub!(:composed_of)
       end
       
       it "should create an Address aggregation" do
         @class.should_receive(:composed_of).with(:address, :mapping => %w(street street2 city state_id zip coords).collect{|x| [x, x.gsub(/_id$/, '')]})
         @class.acts_as_addressed
       end
+      
+      it "should include InstanceMethods in the model" do
+        im = Acts::Addressed::InstanceMethods
+        @class.should_receive(:include).with(im)
+        @class.acts_as_addressed
+      end
+    end
+  end
+end
+
+describe Acts::Addressed::InstanceMethods do
+  before :each do
+    @base = Class.new
+    @base.send :include, Acts::Addressed
+    @class = Class.new @base
+    @class.stub!(:composed_of)
+    @class.acts_as_addressed
+    @model = @class.new
+  end
+  
+  describe "country" do
+    it "should be a valid method" do
+      Acts::Addressed::InstanceMethods.instance_method(:country).should_not be_nil
+    end
+    
+    it "should proxy from address" do
+      @address = mock 'Address'
+      @address.should_receive :country
+      @model.should_receive(:address).and_return(@address)
+      @model.country
     end
   end
 end
