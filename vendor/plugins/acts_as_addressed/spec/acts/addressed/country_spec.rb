39cda94e011a0af9a64add03df882950f6be411c
Marnen Laibow-Koser <marnen@marnen.org>
2009-10-23 01:34:53 -0400

    Improve namespace handling in spec files. [#44]

--- vendor/plugins/acts_as_addressed/spec/acts/addressed/country_spec.rb
+++ vendor/plugins/acts_as_addressed/spec/acts/addressed/country_spec.rb
@@ -2,37 +2,40 @@ require File.dirname(__FILE__) + '/../../spec_helper'
 
 require 'acts/addressed/country'
 
-describe Acts::Addressed::Country, "(associations)" do
-  it "should have many states" do
-    assoc = Acts::Addressed::Country.reflect_on_association(:states)
-    assoc.should_not be_nil
-    assoc.macro.should == :has_many
-  end
-end
-describe Acts::Addressed::Country, "(validations)" do
-  before(:each) do
-    @country = Acts::Addressed::Country.new
-    @country.code = "AA" # arbitrary value of length 2
-    @country.name = "x" # arbitrary value
-  end
-
-  it "should not be valid without a code" do
-    @country.should be_valid
-    @country.code = nil
-    @country.should_not be_valid
+module Acts::Addressed
+  describe Country, "(associations)" do
+    it "should have many states" do
+      assoc = Country.reflect_on_association(:states)
+      assoc.should_not be_nil
+      assoc.macro.should == :has_many
+    end
   end
   
-  it "should not be valid unless length of code is exactly 2" do
-    @country.should be_valid
-    @country.code = "ABC" # length 3
-    @country.should_not be_valid
-    @country.code = "A" # length 1
-    @country.should_not be_valid
-  end
-
-  it "should not be valid without a name" do
-    @country.should be_valid
-    @country.name = nil
-    @country.should_not be_valid
+  describe Country, "(validations)" do
+    before(:each) do
+      @country = Country.new
+      @country.code = "AA" # arbitrary value of length 2
+      @country.name = "x" # arbitrary value
+    end
+  
+    it "should not be valid without a code" do
+      @country.should be_valid
+      @country.code = nil
+      @country.should_not be_valid
+    end
+    
+    it "should not be valid unless length of code is exactly 2" do
+      @country.should be_valid
+      @country.code = "ABC" # length 3
+      @country.should_not be_valid
+      @country.code = "A" # length 1
+      @country.should_not be_valid
+    end
+  
+    it "should not be valid without a name" do
+      @country.should be_valid
+      @country.name = nil
+      @country.should_not be_valid
+    end
   end
 end
