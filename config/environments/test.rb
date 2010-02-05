cc94a0aa08e4ffaa2b8d1e7497a9f57b54b6ce7a
Marnen Laibow-Koser <marnen@marnen.org>
2010-02-04 23:47:59 -0500

    Upgrade RSpec for new version of Rails.

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -22,8 +22,8 @@ config.action_controller.allow_forgery_protection    = false
 config.action_mailer.delivery_method = :test
 
 # Cucumber dependencies.
-config.gem "rspec", :lib => false, :version => ">=1.2.2"  
-config.gem "rspec-rails", :lib => false, :version => ">=1.2.2"  
+config.gem "rspec", :lib => false, :version => ">=1.3.0"  
+config.gem "rspec-rails", :lib => false, :version => ">=1.3.2"  
 config.gem "webrat", :lib => false, :version => ">=0.4.3"  
 config.gem "cucumber", :lib => false, :version => ">=0.2.2"
 
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,18 @@
 # This file is copied to ~/spec when you run 'ruby script/generate rspec'
 # from the project root directory.
-ENV["RAILS_ENV"] = "test"
-require File.expand_path(File.dirname(__FILE__) + "/../config/environment")
-require File.expand_path(File.dirname(__FILE__) + "/blueprints")
-require 'spec'
+ENV["RAILS_ENV"] ||= 'test'
+require File.expand_path(File.join(File.dirname(__FILE__),'..','config','environment'))
+require File.expand_path(File.join(File.dirname(__FILE__), "blueprints"))
+require 'spec/autorun'
 require 'spec/rails'
 include Authlogic::TestCase
+
+# Uncomment the next line to use webrat's matchers
+#require 'webrat/integrations/rspec-rails'
+
+# Requires supporting files with custom matchers and macros, etc,
+# in ./support/ and its subdirectories.
+Dir[File.expand_path(File.join(File.dirname(__FILE__),'support','**','*.rb'))].each {|f| require f}
 
 Spec::Runner.configure do |config|
   # If you're not using ActiveRecord you should remove these
@@ -36,7 +43,7 @@ Spec::Runner.configure do |config|
   #
   # == Mock Framework
   #
-  # RSpec uses it's own mocking framework by default. If you prefer to
+  # RSpec uses its own mocking framework by default. If you prefer to
   # use mocha, flexmock or RR, uncomment the appropriate line:
   #
   # config.mock_with :mocha
@@ -45,7 +52,7 @@ Spec::Runner.configure do |config|
   #
   # == Notes
   # 
-  # For more information take a look at Spec::Example::Configuration and Spec::Runner
+  # For more information take a look at Spec::Runner::Configuration and Spec::Runner
   
   config.before(:each) do
     # Reset Shams for Machinist.
