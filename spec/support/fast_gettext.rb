89ca114c5c8eb713af00e3678069aaa611f40155
Marnen Laibow-Koser <marnen@marnen.org>
2011-09-02 10:38:51 -0400

    Initialize fast_gettext for specs. [#52]

--- spec/support/fast_gettext.rb
+++ spec/support/fast_gettext.rb
@@ -1,2 +1,6 @@
-FastGettext.text_domain ||= SITE_TITLE
-FastGettext.available_locales ||= ['en']
+shared_context 'gettext', :type => 'controller' do
+  before(:all) do
+    FastGettext.text_domain ||= SITE_TITLE
+    FastGettext.available_locales ||= ['en']
+  end
+end
