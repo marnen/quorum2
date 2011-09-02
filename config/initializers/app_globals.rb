13811822f06a727701f3d68be25e61dfc4d7b7f1
Marnen Laibow-Koser <marnen@marnen.org>
2011-09-02 11:11:20 -0400

    Finish setting up FastGettext. [#52]

--- config/initializers/app_globals.rb
+++ config/initializers/app_globals.rb
@@ -4,7 +4,9 @@ require 'yaml'
 APP_CONFIG = YAML.load_file("#{Rails.root}/config/config.yml")[Rails.env]
 
 SITE_TITLE = "Quorum" # Name of site as it appears in <title> element
-FastGettext.add_text_domain SITE_TITLE, :path => File.join(RAILS_ROOT, 'locale')
+FastGettext.add_text_domain SITE_TITLE, :path => File.join(Rails.root, 'locale'), :type => :po
+FastGettext.default_available_locales = ['en'] #all you want to allow
+FastGettext.default_text_domain = SITE_TITLE
 
 DOMAIN = APP_CONFIG['domain'] # Domain on which the site is hosted
 
--- spec/support/fast_gettext.rb
+++
@@ -1,6 +0,0 @@
-shared_context 'gettext', :type => 'controller' do
-  before(:all) do
-    FastGettext.text_domain ||= SITE_TITLE
-    FastGettext.available_locales ||= ['en']
-  end
-end
