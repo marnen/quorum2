35cade0f543aeba072e9ea7892c105372a904bfd
Marnen Laibow-Koser <marnen@marnen.org>
2011-09-02 10:41:50 -0400

    Fix root mapping. [#52]

--- config/routes.rb
+++ config/routes.rb
@@ -64,7 +64,7 @@ Quorum2::Application.routes.draw do
   # just remember to delete public/index.html.
   # root :to => "welcome#index"
 
-  match '/' => 'events#index'
+  root :to => 'events#index'
 
   match 'events/index' => 'events#index'
   
