3043d7347ebc082efaf53717db0d8c45171e2315
Marnen Laibow-Koser <marnen@marnen.org>
2011-09-04 15:47:35 -0400

    Get rid of shams. [#52]

--- spec/support/blueprints.rb
+++ spec/support/blueprints.rb
@@ -10,62 +10,65 @@ LETTERS = ('A'..'Z').to_a
 #     body  { "Lorem ipsum..." }
 #   end
 
+# TODO: Can we use this shams array to cut down on repetition?
+=begin
 shams = {
-  :calendar_name => proc {Faker::Name.name + "'s calendar"},
-  :city => proc {Faker::Address.city},
-  :date => proc {Date.civil(rand(10) + 2000, rand(12) + 1, rand(28) + 1)},
-  :description => proc {Faker::Lorem.paragraph},
-  :email => proc {Faker::Internet.email},
-  :firstname => proc {Faker::Name.first_name},
-  :lastname => proc {Faker::Name.last_name},
-  :generic_name => proc {Faker::Name.last_name},
-  :password => proc {(1..(rand(15) + 4)).map{(32..127).to_a.sample.chr}.join},
-  :street => proc {Faker::Address.street_address},
-  :street2 => proc {Faker::Address.secondary_address},
-  :zip => proc {Faker::Address.zip_code},
-  :code => proc {LETTERS.sample + LETTERS.sample}, # 2 random letters
-  :event_name => proc {Faker::Lorem.words(3).join(' ')}
+  :calendar_name => lambda {Faker::Name.name + "'s calendar"},
+  :city => lambda {Faker::Address.city},
+  :date => lambda {Date.civil(rand(10) + 2000, rand(12) + 1, rand(28) + 1)},
+  :description => lambda {Faker::Lorem.paragraph},
+  :email => lambda {Faker::Internet.email},
+  :firstname => lambda {Faker::Name.first_name},
+  :lastname => lambda {Faker::Name.last_name},
+  :generic_name => lambda {Faker::Name.last_name},
+  :password => lambda {(1..(rand(15) + 4)).map{(32..127).to_a.sample.chr}.join},
+  :street => lambda {Faker::Address.street_address},
+  :street2 => lambda {Faker::Address.secondary_address},
+  :zip => lambda {Faker::Address.zip_code},
+  :code => lambda {LETTERS.sample + LETTERS.sample}, # 2 random letters
+  :event_name => lambda {Faker::Lorem.words(3).join(' ')}
 }
+=end
 
 Event.blueprint do
-  name {shams[:event_name]}
-  description {shams[:description]}
-  street {shams[:street]}
-  street2 {shams[:street2]}
-  city {shams[:city]}
-  state {shams[:state]}
-  zip {shams[:zip]}
-  date {shams[:date]}
-  calendar {shams[:calendar]}
-  created_by {User.make!}
+  name { Faker::Lorem.words(3).join(' ') }
+  description { Faker::Lorem.paragraph }
+  street { Faker::Address.street_address }
+  street2 { Faker::Address.secondary_address }
+  city { Faker::Address.city }
+  state
+  zip { Faker::Address.zip_code }
+  date { Date.civil(rand(10) + 2000, rand(12) + 1, rand(28) + 1) }
+  calendar shams[:calendar]
+  created_by { User.make }
 end
 
 Acts::Addressed::State.blueprint do
-  country {shams[:country]}
-  name {shams[:generic_name]}
-  code {shams[:code]}
+  country
+  name { Faker::Name.last_name } # generic_name
+  code { LETTERS.sample + LETTERS.sample }
 end
 
 Acts::Addressed::Country.blueprint do
-  name {shams[:generic_name]}
-  code {shams[:code]}
+  name lambda {Faker::Name.last_name} # { Faker::Name.last_name } # generic_name
+  code {LETTERS.sample + LETTERS.sample} # { LETTERS.sample + LETTERS.sample }
 end
 
 Calendar.blueprint do
-  name {shams[:calendar_name]}
+  name {Faker::Name.name + "'s calendar"}
 end
 
 User.blueprint do
-  firstname {shams[:firstname]}
-  lastname {shams[:lastname]}
-  email {shams[:email]}
-  password {shams[:password]}
-  password_confirmation {self.password}
-  street {shams[:street]}
-  street2 {shams[:street2]}
-  city {shams[:city]}
-  state {shams[:state]}
-  zip {shams[:zip]}
+  firstname { Faker::Name.first_name }
+  lastname { Faker::Name.last_name }
+  email { Faker::Internet.email }
+  password { (1..(rand(15) + 4)).map{(32..127).to_a.sample.chr}.join }
+  password_confirmation { object.password }
+  street { Faker::Address.street_address }
+  street2 { Faker::Address.secondary_address }
+  city {Faker::Address.city}
+  state 
+  zip { Faker::Address.zip_code }
   active {true}
 end
 
@@ -74,19 +77,19 @@ User.blueprint(:inactive) do
 end
 
 Commitment.blueprint do
-  event {shams[:event]}
-  user {shams[:user]}
+  event 
+  user 
   status {true}
 end
 
 Permission.blueprint do
-  user {shams[:user]}
-  role {shams[:role]}
-  calendar {shams[:calendar]}
+  user
+  role
+  calendar
 end
 
 Permission.blueprint(:admin) do
-  role {Role.make! :admin}
+  role {Role.make :admin}
 end
 
 Role.blueprint do
