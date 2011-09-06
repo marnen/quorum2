9f5b6c2ea12cc7106d7b96e41e1771588953875b
Marnen Laibow-Koser <marnen@marnen.org>
2011-09-06 13:55:31 -0400

    Translate blueprints to FactoryGirl syntax. [#52]

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,14 +1,5 @@
-require 'machinist/active_record'
 require 'ffaker'
 LETTERS = ('A'..'Z').to_a
-
-# Add your blueprints here.
-#
-# e.g.
-#   Post.blueprint do
-#     title { "Post #{sn}" }
-#     body  { "Lorem ipsum..." }
-#   end
 
 # TODO: Can we use this shams array to cut down on repetition?
 =begin
@@ -29,73 +20,74 @@ shams = {
   :event_name => lambda {Faker::Lorem.words(3).join(' ')}
 }
 =end
-
-Event.blueprint do
-  name { Faker::Lorem.words(3).join(' ') }
-  description { Faker::Lorem.paragraph }
-  street { Faker::Address.street_address }
-  street2 { Faker::Address.secondary_address }
-  city { Faker::Address.city }
-  state
-  zip { Faker::Address.zip_code }
-  date { Date.civil(rand(10) + 2000, rand(12) + 1, rand(28) + 1) }
-  calendar shams[:calendar]
-  created_by { User.make }
-end
-
-Acts::Addressed::State.blueprint do
-  country
-  name { Faker::Name.last_name } # generic_name
-  code { LETTERS.sample + LETTERS.sample }
-end
-
-Acts::Addressed::Country.blueprint do
-  name lambda {Faker::Name.last_name} # { Faker::Name.last_name } # generic_name
-  code {LETTERS.sample + LETTERS.sample} # { LETTERS.sample + LETTERS.sample }
-end
-
-Calendar.blueprint do
-  name {Faker::Name.name + "'s calendar"}
-end
-
-User.blueprint do
-  firstname { Faker::Name.first_name }
-  lastname { Faker::Name.last_name }
-  email { Faker::Internet.email }
-  password { (1..(rand(15) + 4)).map{(32..127).to_a.sample.chr}.join }
-  password_confirmation { object.password }
-  street { Faker::Address.street_address }
-  street2 { Faker::Address.secondary_address }
-  city {Faker::Address.city}
-  state 
-  zip { Faker::Address.zip_code }
-  active {true}
-end
-
-User.blueprint(:inactive) do
-  active {false}
-end
-
-Commitment.blueprint do
-  event 
-  user 
-  status {true}
-end
-
-Permission.blueprint do
-  user
-  role
-  calendar
-end
-
-Permission.blueprint(:admin) do
-  role {Role.make :admin}
-end
-
-Role.blueprint do
-  name {'user'}
-end
-
-Role.blueprint :admin do
-  name {'admin'}
+FactoryGirl.define do
+  factory :event do
+    name { Faker::Lorem.words(3).join(' ') }
+    description { Faker::Lorem.paragraph }
+    street { Faker::Address.street_address }
+    street2 { Faker::Address.secondary_address }
+    city { Faker::Address.city }
+    state
+    zip { Faker::Address.zip_code }
+    date { Date.civil(rand(10) + 2000, rand(12) + 1, rand(28) + 1) }
+    calendar
+    created_by { User.make }
+  end
+  
+  factory :state, :class => Acts::Addressed::State do
+    country
+    name { Faker::Name.last_name } # generic_name
+    code { LETTERS.sample + LETTERS.sample }
+  end
+  
+  factory :country, :class => Acts::Addressed::Country do
+    name { Faker::Name.last_name } # generic_name
+    code { LETTERS.sample + LETTERS.sample }
+  end
+  
+  factory :calendar do
+    name {Faker::Name.name + "'s calendar"}
+  end
+  
+  factory :user do
+    firstname { Faker::Name.first_name }
+    lastname { Faker::Name.last_name }
+    email { Faker::Internet.email }
+    password { (1..(rand(15) + 4)).map{(32..127).to_a.sample.chr}.join }
+    password_confirmation { password }
+    street { Faker::Address.street_address }
+    street2 { Faker::Address.secondary_address }
+    city {Faker::Address.city}
+    state 
+    zip { Faker::Address.zip_code }
+    active {true}
+    
+    factory :inactive_user do
+      active {false}
+    end
+  end
+  
+  factory :commitment do
+    event 
+    user 
+    status {true}
+  end
+  
+  factory :permission do
+    user
+    role
+    calendar
+    
+    factory :admin_permission do
+      admin_role
+    end
+  end
+    
+  factory :role do
+    name {'user'}
+    
+    factory :admin_role do
+      name {'admin'}
+    end
+  end
 end
