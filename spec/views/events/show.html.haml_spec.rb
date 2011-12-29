df7395d4e9adcae54b661bfc22ead3d7816026df
Marnen Laibow-Koser <marnen@marnen.org>
2011-12-29 11:49:27 -0500

    Fix bug. [#64]

--- spec/views/events/show.html.haml_spec.rb
+++ spec/views/events/show.html.haml_spec.rb
@@ -6,16 +6,16 @@ describe "/events/show" do
   before(:each) do
     [User, view].each {|x| x.stub!(:current_user).and_return(Factory :user) }
     @event = Factory :event
-    template.stub!(:current_object).and_return(@event)
+    assign :event, @event
   end
-  
+
   it "should render the event and table_header partials" do
     pending "Not sure how to make this work, but it's probably a stupid spec anyway. :)"
     template.should_receive(:render).with(:partial => 'table_header', :locals => {:sortlinks => false})
     template.should_receive(:render).with(:partial => 'event', :locals => {:event => @event})
     render :file => '/events/show'
   end
-  
+
   it "should wrap the whole thing in a <table> of class events" do
     render :file => '/events/show'
     rendered.should =~ %r{^\s*<\s*table\s+([^>]*\s*)class=(["'])events\2[^>]*>.*</table>\s*$}m
