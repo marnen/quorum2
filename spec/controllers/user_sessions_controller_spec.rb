fc1a0edb7867049fd83c246b1b388e193c022cac
Marnen Laibow-Koser <marnen@marnen.org>
2013-08-05 13:48:13 -0400

    Fix whitespace. [#55]

--- spec/controllers/user_sessions_controller_spec.rb
+++ spec/controllers/user_sessions_controller_spec.rb
@@ -9,7 +9,7 @@ describe UserSessionsController do
     UserSession.find.should_not be_nil
     response.should be_redirect
   end
-  
+
   it 'fails login and does not redirect' do
     post :create, :email => 'quentin@example.com', :password => 'bad password'
     UserSession.find.should be_nil
@@ -28,11 +28,11 @@ describe UserSessionsController, 'new (login)' do
   before(:each) do
     get :new
   end
-  
+
   it "should set @page_title" do
     assigns[:page_title].should_not be_nil
   end
-  
+
   it "should render new template" do
     response.should render_template :new
   end
