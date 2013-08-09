6f4e8bf1c53b7ef3c204df5bf0cf8421294949ec
Marnen Laibow-Koser <marnen@marnen.org>
2013-08-08 22:46:26 -0400

    Silence error spew. [#87]

--- ci/create_config_files.sh
+++ ci/create_config_files.sh
@@ -12,7 +12,7 @@ printf >&2 'config/database.yml '
 cp config/config-orig.yml config/config.yml
 printf >&2 'config/config.yml '
 
-cat >config/initializers/secret_token.rb <<EOF
+cat >config/initializers/secret_token.rb 2>/dev/null <<EOF
 Quorum2::Application.config.secret_token = '`bundle exec rake secret`'
 EOF
 printf >&2 'config/initializers/secret_token.rb '
