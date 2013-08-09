83022579f757f043608ce7e4fb771a5809675a08
Marnen Laibow-Koser <marnen@marnen.org>
2013-08-09 00:45:09 -0400

    Make Authlogic work with capybara-webkit on Travis. [#54]

---
+++ features/support/authlogic_webkit_fix.rb
@@ -0,0 +1,16 @@
+# See http://stackoverflow.com/questions/10775472/authlogic-with-capybara-cucumber-selenium-driver-not-working .
+
+module Authlogic
+  module Session
+    module Activation
+      module ClassMethods
+        def controller
+          if !Thread.current[:authlogic_controller]
+            Thread.current[:authlogic_controller] = Authlogic::TestCase::MockController.new
+          end
+          Thread.current[:authlogic_controller]
+        end
+      end
+    end
+  end
+end
