0222abcc9cce8ca2650d596f171ca23ca6274836
Marnen Laibow-Koser <marnen@marnen.org>
2013-08-08 22:50:02 -0400

    Extract function. [#87]

--- ci/create_config_files.sh
+++ ci/create_config_files.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env sh
-printf >&2 'Setting up config files... '
+function warn() {
+  printf >&2 $*
+}
+
+warn 'Setting up config files... '
 cat >config/database.yml <<EOF
 test:
   adapter: postgis
@@ -7,14 +11,14 @@ test:
   username: postgres
   encoding: UTF8
 EOF
-printf >&2 'config/database.yml '
+warn 'config/database.yml '
 
 cp config/config-orig.yml config/config.yml
-printf >&2 'config/config.yml '
+warn 'config/config.yml '
 
 cat >config/initializers/secret_token.rb 2>/dev/null <<EOF
 Quorum2::Application.config.secret_token = '`bundle exec rake secret`'
 EOF
-printf >&2 'config/initializers/secret_token.rb '
+warn 'config/initializers/secret_token.rb '
 
-printf >&2 "done\n"
+warn "done\n"
