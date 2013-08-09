df736bb4c194a2f3f7f3f860530a10735d24bcee
Marnen Laibow-Koser <marnen@marnen.org>
2013-08-08 22:33:40 -0400

    Improve messages. [#87]

--- ci/install_postgis.sh
+++ ci/install_postgis.sh
@@ -3,9 +3,9 @@
 # PostGIS installation stuff from https://github.com/geoalchemy/geoalchemy2/pull/43/files
 # TODO: this could really use some refinement
 
-printf >&2 Installing PostGIS...
+echo >&2 'Installing PostGIS...'
 sudo apt-add-repository -y ppa:sharpie/for-science
 sudo apt-add-repository -y ppa:sharpie/postgis-nightly
 sudo apt-get update
 sudo apt-get install postgresql-9.1-postgis -q
-printf >&2 "done\n"
+echo >&2 'PostGIS installed!'
