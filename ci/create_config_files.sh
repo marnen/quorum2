1972dda53d090349bad8d7fc0afcce81ffe4d592
Marnen Laibow-Koser <marnen@marnen.org>
2013-08-08 22:11:43 -0400

    Move one more task to shell script. [#87]

--- ci/create_config_files.sh
+++ ci/create_config_files.sh
@@ -7,9 +7,14 @@ test:
   username: postgres
   encoding: UTF8
 EOF
-printf >&2 'database.yml '
+printf >&2 'config/database.yml '
 
 cp config/config-orig.yml config/config.yml
-printf >&2 'config.yml '
+printf >&2 'config/config.yml '
+
+cat >config/initializers/secret_token.rb <<EOF
+Quorum2::Application.config.secret_token = '`bundle exec rake secret`'
+EOF
+printf >&2 'config/initializers/secret_token.rb '
 
 printf >&2 "done\n"
