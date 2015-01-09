b2312ce33fb1c5f7dbe2f858598f0864654ad5d0
Marnen Laibow-Koser <marnen@marnen.org>
2015-01-09 17:56:25 -0500

    Upgrade PostGIS adapter. [#84]

--- config/initializers/app_globals.rb
+++ config/initializers/app_globals.rb
@@ -11,6 +11,8 @@ SITE_TITLE = "Quorum" # Name of site as it appears in <title> element
 
 APP_HOME_PAGE = 'http://quorum2.sourceforge.net'
 
+DEFAULT_SRID = 4326 # SRID for terrestrial coordinates
+
 DOMAIN = APP_CONFIG['domain'] # Domain on which the site is hosted
 
 EMAIL = APP_CONFIG['email'] # Address that application-generated e-mail will come from.
