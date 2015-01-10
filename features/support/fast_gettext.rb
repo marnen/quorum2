be74e4cd73e50c8351b68f2484d69d08af3f9d33
Marnen Laibow-Koser <marnen@marnen.org>
2015-01-10 01:00:24 -0500

    Remove obsolete loading of debugger. [#84]

--- features/support/fast_gettext.rb
+++ features/support/fast_gettext.rb
@@ -1,11 +1,5 @@
 # coding: UTF-8
 
-begin
-  require 'ruby-debug'
-rescue LoadError
-  warn 'Failed to load ruby-debug; continuing without it...'
-end
-
 # Set Gettext stuff so we can load Web pages.
 FastGettext.text_domain ||= SITE_TITLE
 FastGettext.available_locales ||= ['en']
