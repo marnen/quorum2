f597978290aad94f604672b15d762a3752030619
Marnen Laibow-Koser <marnen@marnen.org>
2015-01-12 15:06:49 -0500

    Use let blocks to clean up spec. [#84]

--- spec/lib/resource_params_spec.rb
+++ spec/lib/resource_params_spec.rb
@@ -3,18 +3,15 @@ require 'resource_params'
 
 describe ResourceParams do
   describe '#resource_params' do
+    let(:resource_name) { Faker::Lorem.words(1).first.singularize }
+    let(:model_class) { Class.new }
+    let(:model_name) { resource_name.titleize }
+    let(:controller_class) { Class.new ApplicationController }
+    let(:controller_name) { "#{model_name.pluralize}Controller" }
+    let(:permitted_params) { Faker::Lorem.words.map &:to_sym }
+
     it "returns the model's list of permitted params" do
-      resource_name = Faker::Lorem.words(1).first.singularize
-
-      model_name = resource_name.titleize
-
-      model_class = Class.new
-
-      controller_name = "#{model_name.pluralize}Controller"
-      controller_class = Class.new ApplicationController
       controller_class.class_eval { include ResourceParams }
-
-      permitted_params = Faker::Lorem.words.map &:to_sym
       model_class.stub permitted_params: permitted_params
 
       with_consts controller_name => controller_class, model_name => model_class do
