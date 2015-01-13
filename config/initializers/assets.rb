f6d27ea59dce7c4b8b2323ecce39cb1ebb9b5a66
Marnen Laibow-Koser <marnen@marnen.org>
2015-01-13 16:29:22 -0500

    Fix asset compilation code.

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,14 +1,14 @@
-# Adapted from http://guides.rubyonrails.org/asset_pipeline.html#precompiling-assets
+# Adapted from http://guides.rubyonrails.org/v4.0.2/asset_pipeline.html#precompiling-assets
 
 Rails.application.config.assets.precompile << Proc.new do |path|
-  if path =~ /\a[^_].*\.(css|js)\z/
+  if path =~ %r{\A[^_].*\.(css|js)\Z}
     full_path = Rails.application.assets.resolve(path).to_path
     app_assets_path = Rails.root.join('app', 'assets').to_path
     if full_path.starts_with? app_assets_path
-      logger.info "including asset: " + full_path
+      puts "including asset: " + full_path
       true
     else
-      logger.info "excluding asset: " + full_path
+      puts "excluding asset: " + full_path
       false
     end
   else
