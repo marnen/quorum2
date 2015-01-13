d9d0603f5192dc329bdcc53607c19c4dcb96dc5e
Marnen Laibow-Koser <marnen@marnen.org>
2015-01-13 17:08:17 -0500

    Switch CSS compressor.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -56,7 +56,7 @@ Quorum2::Application.configure do
 
   # Compress JavaScripts and CSS
   config.assets.js_compressor = :uglifier
-  config.assets.css_compressor = :yui
+  config.assets.css_compressor = :sass
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
